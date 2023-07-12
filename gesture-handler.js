9c51a403f01298978f5c4f38fff9aff6fe5c78e4
yosr-zouaoui <zwawiyosr@gmail.com>
2023-07-12 02:14:56 +0200

    change

--- gesture-handler.js
+++ gesture-handler.js
@@ -10,7 +10,7 @@ AFRAME.registerComponent("gesture-handler", {
   
     init: function () {
       this.handleScale = this.handleScale.bind(this);
-      this.handleRotation = this.handleRotation.bind(this);
+      this.handlePosition = this.handlePosition.bind(this);
   
       this.isVisible = false;
       this.initialScale = this.el.object3D.scale.clone();
@@ -30,25 +30,25 @@ AFRAME.registerComponent("gesture-handler", {
   
     update: function () {
       if (this.data.enabled) {
-        this.el.sceneEl.addEventListener("onefingermove", this.handleRotation);
+        this.el.sceneEl.addEventListener("onefingermove", this.handlePosition);
         this.el.sceneEl.addEventListener("twofingermove", this.handleScale);
       } else {
-        this.el.sceneEl.removeEventListener("onefingermove", this.handleRotation);
+        this.el.sceneEl.removeEventListener("onefingermove", this.handlePosition);
         this.el.sceneEl.removeEventListener("twofingermove", this.handleScale);
       }
     },
   
     remove: function () {
-      this.el.sceneEl.removeEventListener("onefingermove", this.handleRotation);
+      this.el.sceneEl.removeEventListener("onefingermove", this.handlePosition);
       this.el.sceneEl.removeEventListener("twofingermove", this.handleScale);
     },
   
-    handleRotation: function (event) {
+    handlePosition: function (event) {
       if (this.isVisible) {
-        this.el.object3D.rotation.y +=
-          event.detail.positionChange.x * this.data.rotationFactor;
-        this.el.object3D.rotation.x +=
-          event.detail.positionChange.y * this.data.rotationFactor;
+        this.el.object3D.position.y +=
+          event.detail.positionChange.x * this.data.positionFactor;
+        this.el.object3D.position.x +=
+          event.detail.positionChange.y * this.data.positionFactor;
       }
     },
   
